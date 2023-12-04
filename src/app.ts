03bfddc6565da0b9384c715d8fab0a01c734338e
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-12-04 14:35:58 +0600

    Basic setup done

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import express, { Request, Response } from "express";
+const app = express();
+// require("dotenv").config();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import dotenv from "dotenv";
+import cors from "cors";
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Sending response to Node.js Express application");
+});
+
+// console.log(process.cwd()); //not show cwd
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+/* All the activity of run server is here */
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// console.log(process.cwd()); //not show cwd
+
+main();
